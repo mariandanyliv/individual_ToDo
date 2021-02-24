37ca2db0d0f6bcacd17c77661ed41da8aeebb1b5
mariandanyliv <danulivmarian40@gmail.com>
2021-02-24 16:14:06 +0200

    connect css

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="./css/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    sdafgsdfg
+    <div class="container"><h1>ToDo_List</h1></div>
 </body>
 </html>
