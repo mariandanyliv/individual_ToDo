9a5c3fbe3d43da95423027556a8884175fbdd250
mariandanyliv <danulivmarian40@gmail.com>
2021-02-24 20:27:02 +0200

    add main front-end

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="./css/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>ToDo</title>
 </head>
 <body>
-    <div class="container"><h1>ToDo_List</h1></div>
+    <div class="container">
+        <h2 class="title">ToDo List</h2>
+        <div class="input">
+            <input type="text" placeholder="Enter your task">
+            <button type="submit" class="btn"><i class="fa fa-plus"></i></button>
+        </div>
+        <div class="content">
+            <ul>
+                <li>
+                    <span class="text">asdfasdf</span>
+                    <i class="icon fa fa-trash"></i>
+                </li>
+                <li>
+                    <span class="text">asdfasdf</span>
+                    <i class="icon fa fa-trash"></i>
+                </li>
+                <li>
+                    <span class="text">asdfasdf</span>
+                    <i class="icon fa fa-trash"></i>
+                </li>
+                <li>
+                    <span class="text">asdfasdf</span>
+                    <i class="icon fa fa-trash"></i>
+                </li>
+                <dic class="pending-text">dbfdfbd 4</dic>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
